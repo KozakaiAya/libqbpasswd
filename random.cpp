94d7b2cdd9191c53e62759723fa126836f25a8b6
KozakaiAya <zju3150105246@outlook.com>
2019-12-18 21:20:52 -0500

    random salt generator

---
+++ random.cpp
@@ -0,0 +1,13 @@
+#include "random.h"
+
+#include <random>
+
+uint32_t rand(uint32_t min, uint32_t max)
+{
+    std::random_device rd;  // No need to follow the qb's original /dev/urandom implementation
+                            // The std::random_device already comes with a device-dependent implemenration
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<uint32_t> uniform(min, max);
+
+    return uniform(gen);
+}
---
+++ random.h
@@ -0,0 +1,5 @@
+#include <cstdint>
+#include <limits>
+
+uint32_t rand(uint32_t min = 0, uint32_t max = std::numeric_limits<uint32_t>::max());
+
