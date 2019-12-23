5064fb3b6b26a8df9af24dc371dc72b4d43acaf3
KozakaiAya <37061278+KozakaiAya@users.noreply.github.com>
2019-12-23 13:13:19 -0500

    fix typo

--- random.cpp
+++ random.cpp
@@ -5,7 +5,7 @@
 uint32_t get_rand(uint32_t min, uint32_t max)
 {
     std::random_device rd;  // No need to follow the qb's original /dev/urandom implementation
-                            // The std::random_device already comes with a device-dependent implemenration
+                            // The std::random_device already comes with a device-dependent one
     std::mt19937 gen(rd());
     std::uniform_int_distribution<uint32_t> uniform(min, max);
 
