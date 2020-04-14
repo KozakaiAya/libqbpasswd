3f7dc91a2875f7619f0f68aa71846246591648c2
KozakaiAya <37061278+KozakaiAya@users.noreply.github.com>
2020-04-14 15:04:14 -0400

    update openssl build script

--- build_openssl.sh
+++ build_openssl.sh
@@ -4,6 +4,7 @@ curr_dir=$(pwd)
 openssl_build_dir="$curr_dir/build"
 mkdir $openssl_build_dir
 
+cd $curr_dir/openssl
 $curr_dir/openssl/config --prefix="$openssl_build_dir" threads no-shared no-dso no-comp CXXFLAGS="-std=c++14 -ffunction-sections -fdata-sections" LDFLAGS="-Wl,--no-as-needed -ldl -lpthread -pthread"
 make -j$(nproc)
 make install_sw install_ssldirs
