644231fb72aa975c0988984521cebdde6089adeb
KozakaiAya <37061278+KozakaiAya@users.noreply.github.com>
2020-04-14 15:32:58 -0400

    check argument input

--- main.cpp
+++ main.cpp
@@ -8,6 +8,12 @@
 
 int main(int argc, char* argv[])
 {
+    if (argc != 2)
+    {
+        std::cout << "Usage: qb_password_gen(_static) <your plain password>" << std::endl;
+        std::exit(-1);
+    }
+
     std::string password = argv[1];
 
     Password encoder(100000, EVP_sha512());
